5025b39cad7e39970de9d158b96f57fc4e64721c
Eric Dasse <christ.gououi@gmail.com>
2022-12-07 15:31:59 +0100

    Add docstring

--- knowledge_extraction.py
+++ knowledge_extraction.py
@@ -6,5 +6,14 @@ nltk.download("punkt")
 
 
 def get_entity_pairs(sentence):
+    """Get the subject and the object of a sentence. They represent entities of a potential
+    graph
+
+    Args:
+        sentence (str): sentence
+
+    Returns:
+        tuple: entities pair
+    """
     tokens = word_tokenize(sentence)
     return (tokens[0], tokens[-1])
